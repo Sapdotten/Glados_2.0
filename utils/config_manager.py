29f8545cdea3b4afe990ae0f1d2123f8cb77e8db
Sapdotten <semenova.a.p.04@gmail.com>
2024-02-24 03:08:53 +0400

    Сделаны конфиги с данными бд

--- utils/config_manager.py
+++ utils/config_manager.py
@@ -12,18 +12,48 @@ class Configs:
 
 
 class Logger(Configs):
+    config_name = 'logging'
+
     @classmethod
     def is_console(cls) -> int:
-        return cls.configs['logging']['console']
+        return cls.configs[cls.config_name]['console']
 
     @classmethod
     def file(cls) -> str:
-        return cls.configs['logging']['file']
+        return cls.configs[cls.config_name]['file']
 
     @classmethod
     def level(cls) -> str:
-        return cls.configs['logging']['level']
+        return cls.configs[cls.config_name]['level']
+
+
+class Database(Configs):
+    config_name = 'database'
+
+    @classmethod
+    def user(cls):
+        return cls.configs[cls.config_name]['user']
+
+    @classmethod
+    def name(cls):
+        return cls.configs[cls.config_name]['base_name']
+
+    @classmethod
+    def password(cls):
+        return cls.configs[cls.config_name]['password']
+
+    @classmethod
+    def host(cls):
+        return cls.configs[cls.config_name]['host']
+
+    @classmethod
+    def port(cls):
+        return cls.configs[cls.config_name]['port']
 
 
 def start():
     Configs.init_configs()
+
+
+start()
+print(Database.user())
